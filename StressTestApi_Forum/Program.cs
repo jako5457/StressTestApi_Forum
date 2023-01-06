8555ed6ffe8f7ebd4daf1c5a0f7e9e3977538360
jako5457 <jako5457@gmail.com>
2023-01-06 09:18:21 +0100

    added robots.txt

--- StressTestApi_Forum/Program.cs
+++ StressTestApi_Forum/Program.cs
@@ -1,5 +1,7 @@
 using StressTestApi_Forum.Middleware;
 using StressTestApi_Forum.Services;
+using RobotsTxt;
+using StressTestApi_Forum;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,8 +12,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IRobotsTxtProvider, RobotTxtProvider>();
+
 
 var app = builder.Build();
+
+app.UseRobotsTxt();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ StressTestApi_Forum/RobotTxtProvider.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Options;
+using RobotsTxt;
+using System.Text;
+
+namespace StressTestApi_Forum
+{
+    public class RobotTxtProvider : IRobotsTxtProvider
+    {
+        private readonly IConfiguration _Configuration;
+        private readonly ILogger<RobotTxtProvider> _Logger;
+        private readonly IHostEnvironment _Environment;
+
+        public RobotTxtProvider(IConfiguration configuration,ILogger<RobotTxtProvider> logger,IHostEnvironment environment)
+        {
+            _Configuration = configuration;
+            _Logger = logger;
+            _Environment = environment;
+        }
+
+        public async Task<RobotsTxtResult> GetResultAsync(CancellationToken cancellationToken)
+        {
+            _Logger.LogInformation("A bot has requested robots.txt");
+
+            var builder = new RobotsTxtOptionsBuilder();
+
+            if (_Environment.IsDevelopment())
+            {
+                builder = builder.AddSection(options => options
+                                                            .AddComment("This is a development build.")
+                                                            .AddUserAgent("*")
+                                                            .Allow("/"));
+            }
+            else
+            {
+                builder = builder
+                    .AddSection(options => options
+                                    .AddComment("Allow google to crawl the website.")
+                                    .AddUserAgent("Googlebot")
+                                    .Allow("/")
+                )
+                    .DenyAll();
+            }
+
+            var options = builder.Build();
+            var content = options.ToString();
+            var buffer = Encoding.UTF8.GetBytes(content).AsMemory();
+            return new RobotsTxtResult(buffer,10000);
+        }
+    }
+}
